37c9cb491a2a4fe33523da924adb590c498817ad
daniloguimaraes <guimaraesdjl@gmail.com>
2015-08-11 19:20:50 -0300

    Hello. Hello, computer!

---
+++ app/js/app-config.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Danilo on 10/08/2015.
+ */
+
+angular.module('learningAngularJs', [])
+    .controller('HelloWorldController', HelloWorldController
+);
+
+HelloWorldController.$inject = ['$scope'];
+
+function HelloWorldController($scope) {
+    $scope.myStyle = {};
+    $scope.myStyle.width = '100px';
+    $scope.myStyle.height = '100px';
+    $scope.myClass = '';
+
+    $scope.$watch('nome', function(newValue, oldValue) {
+        if (newValue == oldValue) {
+            return;
+        }
+
+        if (angular.isDefined(newValue) && newValue == 'oobj') {
+            $scope.myStyle.backgroundColor = 'green';
+            $scope.myClass = 'red';
+        } else {
+            $scope.myStyle.backgroundColor = 'yellow';
+            $scope.myClass = 'blue';
+        }
+    });
+
+}
